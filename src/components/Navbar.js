96a6932e531d571f4f6d8996b82abb81b531223e
Gowtham9503 <gowtham.090503@gmail.com>
2024-12-20 09:14:52 +0530

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import './Navbar.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
     const navigate = useNavigate();
     const userString = localStorage.getItem('user');
-    const user = userString?JSON.parse(userString):null;
-    console.log(user)
+    const user = userString ? JSON.parse(userString) : null;
+    const [isOpen, setIsOpen] = useState(false);
 
     const handleLogout = () => {
         // Remove user data from local storage
@@ -15,14 +15,18 @@ const Navbar = () => {
         navigate('/');
     };
 
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">CollabTool</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/dashboard">Dashboard</Link>
@@ -31,7 +35,7 @@ const Navbar = () => {
                     {user ? (
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <button className="btn btn-link nav-link" onClick={handleLogout}>{user.username}  Logout</button>
+                                <button className="btn btn-link nav-link" onClick={handleLogout}>{user.username} Logout</button>
                             </li>
                         </ul>
                     ) : (
