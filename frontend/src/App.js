4e8a63e7c7138b49068ede3b2d4b29b80a70543c
Gowtham9503 <gowtham.090503@gmail.com>
2024-12-26 18:30:10 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Register from './components/Register';
+import DocumentEditor from './components/DocumentEditor';
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/documents/:id" element={<DocumentEditor />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
+};
+
+export default App;
